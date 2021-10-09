ff7d3deb4c0d62a5b2d8943716c19d7899b93759
Andrey <andrej942011@gmail.com>
2021-10-09 13:30:26 +0500

    Изменено отображение выходного массива

--- FormalNeuronDemo/Program.cs
+++ FormalNeuronDemo/Program.cs
@@ -9,9 +9,9 @@ namespace FormalNeuronDemo
             //перевод четных и не четных двоичных чисел
 
             Neuron neuron = new Neuron(LearnDate.X, LearnDate.Y);
-            Console.WriteLine($"[{string.Join(",", neuron.W)}] {neuron.C}");
+            Console.WriteLine($"[{string.Join(", ", neuron.W)}] {neuron.C}");
             foreach (int[] test in LearnDate.Test)
-                Console.WriteLine($"[{string.Join(",", test)} {test[3]} {neuron.calculate(test)}]");
+                Console.WriteLine($"[{string.Join(", ", test)} {test[3]} {neuron.calculate(test)}]");
 
             return 0;
 
